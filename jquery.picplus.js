ac79cc1d628cb0defbbd55a28a7b5914b896fe60
Matthew Dapena-Tretter <m@tthewwithanm.com>
2013-11-14 20:04:08 -0500

    Ignore missing methods

--- jquery.picplus.js
+++ jquery.picplus.js
@@ -114,7 +114,9 @@
         _callPluginMethod: function (method) {
             var args = Array.prototype.slice.call(arguments, 1);
             $.each(this.options.plugins, function (i, plugin) {
-                plugin[method].apply(plugin, args);
+                if (typeof plugin[method] === 'function') {
+                    plugin[method].apply(plugin, args);
+                }
             });
         },
 
